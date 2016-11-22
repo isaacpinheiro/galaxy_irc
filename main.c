70c91f0cb9350fe4b968d2a5a5bf7a1df92e066c
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-11-22 00:31:49 -0200

    Implementando o client TCP.

--- main.c
+++ main.c
@@ -7,8 +7,43 @@
 
 #include "client_cmd.h"
 
+#define BUFFER_SIZE 1024
+
 int main()
 {
+
+    int sock;
+    struct sockaddr_in server_addr;
+    unsigned short server_port = 9000;
+    char server_ip[16] = "127.0.0.1";
+    char buffer[BUFFER_SIZE];
+
+    if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror("socket() failed!");
+        exit(-1);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+    server_addr.sin_port = htons(server_port);
+
+    if (connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        perror("connect() failed!");
+        exit(-1);
+    }
+
+    while (1) {
+
+        scanf("%s", buffer);
+        send(sock, buffer, sizeof(buffer), 0);
+
+        //recv(sock, buffer, sizeof(buffer), 0);
+        //printf("%s\n\n", buffer);
+
+    }
+
+    close(sock);
 
     return 0;
 
