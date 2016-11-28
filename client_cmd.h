689c6a2941340560b6fcf27419279d39c67fa08c
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-11-28 17:45:54 -0200

    Mudando o BUFFER_SIZE para o arquivo 'client_cmd.h'.

--- client_cmd.h
+++ client_cmd.h
@@ -1,6 +1,8 @@
 #ifndef CLIENT_CMD_H
 #define CLIENT_CMD_H
 
+#define BUFFER_SIZE 1024
+
 void strip(char *str);
 
 #endif
--- main.c
+++ main.c
@@ -7,8 +7,6 @@
 
 #include "client_cmd.h"
 
-#define BUFFER_SIZE 1024
-
 int main(int argc, char **argv)
 {
 
