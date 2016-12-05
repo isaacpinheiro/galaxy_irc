dda56e9a7329becc076666b2744890ca1b791d0c
geneglauber <lucasmazim@yahoo.com.br>
2016-12-05 03:04:36 +0000

    Estrutura Message Funcionando

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ int main(int argc, char **argv)
     char server_ip[16];
     char buffer[BUFFER_SIZE];
     strcat(server_ip, argv[1]);
+    Message *Me = (Message*) malloc(sizeof(Message));
 
     if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
         perror("socket() failed!");
@@ -39,16 +40,30 @@ int main(int argc, char **argv)
     new_server_sock = malloc(1);
     *new_server_sock = sock;
 
+    // inicia aqui pegando um nick
+    printf("Escolha um nick: ");
+    fgets(buffer, 256, stdin);
+    strip(buffer);
+    strcat(Me->user_name, buffer);
+    
     pthread_create(&server_thread, NULL, server_handler, (void*) new_server_sock);
-
+    
+    
     while (1) {
-
         buffer[0] = '\0';
+        memset(Me->content, '\0', 512);
+        memset(Me->command, '\0', 256);
+        
         fgets(buffer, 512, stdin);
         strip(buffer);
-        send(sock, buffer, sizeof(buffer), 0);
+        xxx(buffer, Me);
+        
+        // nisso eu nao mexi nada, nao entendi bem como funcionava esse send e o recv
+        char *buff = (char*) Me;    
+        send(sock, buff, sizeof(buff), 0);
 
     }
+        
 
     close(sock);
 
