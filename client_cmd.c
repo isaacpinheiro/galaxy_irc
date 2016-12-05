659583f5cc998ece4ac537d1d18f62925ca6a877
geneglauber <lucasmazim@yahoo.com.br>
2016-12-05 03:04:58 +0000

    Estrutura Message Funcionando

--- client_cmd.c
+++ client_cmd.c
@@ -5,12 +5,42 @@
 
 #include "client_cmd.h"
 
+void xxx (char *buffer, Message *Me) {
+    int i = 0;
+     
+    if (buffer[0] == '/') {
+        buffer++;
+        
+        while (*buffer != ' ') {
+            Me->command[i] = *buffer;
+            i++;
+            ++buffer;
+        }
+            
+        i=0;
+        buffer++;
+        while (*buffer != '\0') {
+            Me->content[i] = *buffer;
+            i++;
+            ++buffer;
+        }
+            
+    } else {
+        strcat(Me->content, buffer);
+    }
+    
+    printf("Nome: %s  Comando: %s  Mensagem: %s\n", Me->user_name, Me->command, Me->content);
+        
+}
+
 void strip(char *str)
 {
     int l = strlen(str) - 1;
     if (str[l] == '\n') str[l] = '\0';
 }
 
+
+//tentei nao mexer em nada aqui
 void *server_handler(void *server_sock)
 {
 
@@ -19,9 +49,11 @@ void *server_handler(void *server_sock)
     char buffer[BUFFER_SIZE];
 
     while ((read_size = recv(sock, buffer, sizeof(buffer), 0)) > 0) {
-        printf("%s\n\n", buffer);
-        buffer[0] = '\0';
+        
+        memset(buffer, '\0', 512);
     }
 
 }
+
+
 
--- client_cmd.h
+++ client_cmd.h
@@ -3,8 +3,17 @@
 
 #define BUFFER_SIZE 1024
 
+typedef struct Message{
+    char user_name[256];
+    char command[256];
+    char content[512];
+} Message;
+
 void strip(char *str);
 void *server_handler(void *server_sock);
+void xxx (char *buffer, Message *Me);
+
+
 
 #endif
 
